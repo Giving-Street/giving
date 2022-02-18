ad42126f3d4b823c3bddeb7db45b4eecdd91a757
humphrey <ahnsv@bc.edu>
2022-02-18 23:13:49 +0900

    refactor(giving-account): replace startup event with depends function (#2)

--- giving-account/giving_account/routers/google_auth/router.py
+++ giving-account/giving_account/routers/google_auth/router.py
@@ -1,42 +1,48 @@
+from typing import Callable
+
 from authlib.integrations.base_client import OAuthError
-from fastapi import APIRouter, Request, status, HTTPException
+from fastapi import APIRouter, Request, status, HTTPException, Depends
 from fastapi.responses import HTMLResponse, RedirectResponse
 from starlette.config import Config
+from giving_account.config import Config as GivingAccountConfig
 from authlib.integrations.starlette_client import OAuth
 
 google_auth_router = APIRouter(prefix="/auth/google")
 
 
-@google_auth_router.on_event("startup")
-async def startup():
-    from giving_account.app import app
-    config = Config(environ={
-        "GOOGLE_CLIENT_ID": app.state.config.google_oauth.google_client_id,
-        "GOOGLE_CLIENT_SECRET": app.state.config.google_oauth.google_client_secret
-    })
-    oauth = OAuth(config=config)
-    conf_url = 'https://accounts.google.com/.well-known/openid-configuration'
-    oauth.register(
-        name='google',
-        server_metadata_url=conf_url,
-        client_kwargs={
-            'scope': 'openid email profile'
-        },
-    )
-    app.state.google_oauth = oauth
+async def get_oauth():
+    def _get_oauth(config: GivingAccountConfig) -> OAuth:
+        config = Config(environ={
+            "GOOGLE_CLIENT_ID": config.google_oauth.google_client_id,
+            "GOOGLE_CLIENT_SECRET": config.google_oauth.google_client_secret
+        })
+        oauth = OAuth(config=config)
+        conf_url = 'https://accounts.google.com/.well-known/openid-configuration'
+        oauth.register(
+            name='google',
+            server_metadata_url=conf_url,
+            client_kwargs={
+                'scope': 'openid email profile'
+            },
+        )
+        return oauth
+
+    return _get_oauth
 
 
 @google_auth_router.get("/login")
-async def login(request: Request):
+async def login(request: Request, get_oauth_: Callable[..., OAuth] = Depends(get_oauth)):
+    oauth = get_oauth_(request.app.state.config)
     redirect_uri = request.url_for('auth')
-    redirect_response = await request.app.state.google_oauth.google.authorize_redirect(request, redirect_uri)
+    redirect_response = await oauth.google.authorize_redirect(request, redirect_uri)
     return redirect_response
 
 
 @google_auth_router.get('/auth')
-async def auth(request: Request):
+async def auth(request: Request, get_oauth_: Callable[..., OAuth] = Depends(get_oauth)):
+    oauth = get_oauth_(request.app.state.config)
     try:
-        token = await request.app.state.google_oauth.google.authorize_access_token(request)
+        token = await oauth.google.authorize_access_token(request)
     except OAuthError as error:
         return HTMLResponse(f'<h1>{error.error}</h1>', status_code=status.HTTP_401_UNAUTHORIZED)
     user = token.get('userinfo')
